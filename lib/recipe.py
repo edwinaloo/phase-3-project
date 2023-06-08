8571e6ac84c6f5557a21d4c3c44d2fb09aa0c2d3
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-06-08 19:46:49 +0300

    push

--- lib/recipe.py
+++ lib/recipe.py
@@ -122,7 +122,7 @@ def delete_recipe():
 # Function to search for recipes by name
 def search_recipes():
     keyword = input("Enter a keyword to search for: ")
-    query = "SELECT * FROM recipes WHERE id IN (SELECT recipe_id FROM search_terms WHERE term LIKE ?)"
+    query = "SELECT * FROM recipes WHERE id IN (SELECT Recipe_ID FROM search_terms WHERE term LIKE ?)"
     cursor.execute(query, ('%' + keyword.lower() + '%',))
     recipes = cursor.fetchall()
 
